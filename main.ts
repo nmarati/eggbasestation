56383354e50d2afa2864f2520c9a45154b19d864
nmarati <narayanan.marati@gmail.com>
2021-09-26 20:39:03 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
-    serial.writeValue("s", 0)
     collect = 1
     radio.sendString("start")
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Happy)
-    serial.writeValue("s", 0)
 })
 radio.onReceivedString(function (receivedString) {
     if (collect == 1) {
@@ -15,7 +13,6 @@ radio.onReceivedString(function (receivedString) {
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.No)
-    serial.writeValue("s", 0)
     collect = 0
 })
 let collect = 0
